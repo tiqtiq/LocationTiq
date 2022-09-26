57bf5a56b6ae00598fbbb4c6633fa1643bc0efed
marcprux <mwp1@cornell.edu>
2022-09-26 13:19:21 -0400

    chore: checkpoint

--- Tests/LocationTiqTests/LocationTiqTests.swift
+++ Tests/LocationTiqTests/LocationTiqTests.swift
@@ -4,4 +4,25 @@ import LocationTiq
 final class LocationTiqTests: XCTestCase {
     func testLocation() async throws {
     }
+
+    #if canImport(ObjectiveC) // needed for AutoreleasingUnsafeMutablePointer
+    func testLocationFormatter() async throws {
+        let ddcompact = {
+            LocationDegreesFormatter(format: .decimalDegrees, displayOptions: .compact)!.string(from: $0)
+        }
+        XCTAssertEqual("0.0°", ddcompact(0))
+        XCTAssertEqual("120.33°", ddcompact(120.33))
+        XCTAssertEqual("-179.0°", ddcompact(-179))
+        XCTAssertEqual(nil, ddcompact(-312.5434224))
+
+        let degminsec = {
+            LocationDegreesFormatter(format: .degreesMinutesSeconds, displayOptions: .compact)!.string(from: $0)
+        }
+        XCTAssertEqual("0°0\'0\"", degminsec(0))
+        XCTAssertEqual("120°19\'48\"", degminsec(120.33))
+        XCTAssertEqual("-179°0\'0\"", degminsec(-179))
+        XCTAssertEqual(nil, degminsec(-312.5434224))
+
+    }
+    #endif
 }
